ac6b7a450c7d9e3dfc2afefa4b630e8046bf355a
KmYongHwan <kyh5678@gmail.com>
2021-10-28 19:08:33 +0900

    대략적인 ui

--- app/src/main/java/com/example/give_me_drive_music/MainActivity.java
+++ app/src/main/java/com/example/give_me_drive_music/MainActivity.java
@@ -1,14 +1,78 @@
 package com.example.give_me_drive_music;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false); // 기존 title 지우기
+        actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼 만들기
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                menuItem.setChecked(true);
+                mDrawerLayout.closeDrawers();
+
+                int id = menuItem.getItemId();
+                String title = menuItem.getTitle().toString();
+
+                if(id == R.id.survey){
+                    Toast.makeText(context, title + ": 설문조사 화면으로 이동합니다.", Toast.LENGTH_SHORT).show();
+                }
+
+                else if(id == R.id.feedback){
+                    Toast.makeText(context, title + ": 피드백 현황 화면으로 이동합니다", Toast.LENGTH_SHORT).show();
+                }
+
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.setting:
+                Toast.makeText(getApplicationContext(), "설정 화면입니다.", Toast.LENGTH_LONG).show();
+
+            case android.R.id.home: //toolbar의 back키 눌렀을 때 동작
+                Toast.makeText(getApplicationContext(), "뒤로 가기 버튼이 눌렸습니다.", Toast.LENGTH_LONG).show();
+                //finish();
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
