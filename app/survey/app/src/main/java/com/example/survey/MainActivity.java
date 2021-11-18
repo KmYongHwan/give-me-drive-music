0cace201b1abd21184c4e346e98c1248c005ee9b
KmYongHwan <kyh5678@gmail.com>
2021-11-18 19:32:12 +0900

    앱 음성인식 적용

--- app/survey/app/src/main/java/com/example/survey/MainActivity.java
+++ app/survey/app/src/main/java/com/example/survey/MainActivity.java
@@ -1,18 +1,23 @@
 package com.example.survey;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -25,15 +30,16 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
+    private Button btVoice;
+    private TextView txVoice;
+    private final int myVoiceCode = 1234;
 
     private DatabaseReference mPostReference;
-
-    static ArrayList<String> arrayIndex =  new ArrayList<String>();
-    static ArrayList<String> arrayData = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +48,40 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        btVoice = findViewById(R.id.voice);
+        txVoice = findViewById(R.id.textView);
+
+
+        btVoice.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startVoiceIntent();
+            }
+        });
+    }
+
+    protected void startVoiceIntent(){
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.KOREAN);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "인식 중 입니다...");
+        startActivityForResult(intent, myVoiceCode);
+    }
+
+    protected String getRecogStr(Intent data){
+        ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+        return result.get(0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == myVoiceCode && resultCode == RESULT_OK && data != null){
+            String msg = getRecogStr(data);
+            //txVoice.setText(msg);
+            FuncVoiceOrderCheck(msg);
+        }
     }
 
     @Override
@@ -51,6 +91,16 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    private void FuncVoiceOrderCheck(String VoiceMsg){
+        if(VoiceMsg.length() < 1) return;
+        if(VoiceMsg.indexOf("메뉴") > -1){
+            finish();
+            Intent intent = new Intent(getApplicationContext(), SurveyActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         //return super.onOptionsItemSelected(item);
         switch (item.getItemId()) {
