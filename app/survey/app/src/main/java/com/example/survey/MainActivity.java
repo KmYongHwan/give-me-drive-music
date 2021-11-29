967f723aa8ef34350250905535a812f977aa5a81
KmYongHwan <kyh5678@gmail.com>
2021-11-29 15:18:19 +0900

    11/29 카메라 저장은 x

--- app/survey/app/src/main/java/com/example/survey/MainActivity.java
+++ app/survey/app/src/main/java/com/example/survey/MainActivity.java
@@ -4,9 +4,18 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.hardware.camera2.CameraDevice;
 import android.content.Intent;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Handler;
 import android.speech.RecognizerIntent;
@@ -14,16 +23,22 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -51,7 +66,18 @@ import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback{
+
+    private static final String TAG = "android_camera_example";
+    private static final int PERMISSIONS_REQUEST_CODE = 100;
+    String[] REQUIRED_PERMISSIONS  = {android.Manifest.permission.CAMERA,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    private static final int CAMERA_FACING = Camera.CameraInfo.CAMERA_FACING_BACK; // Camera.CameraInfo.CAMERA_FACING_FRONT
+
+    private SurfaceView surfaceView;
+    private CameraPreview mCameraPreview;
+    private View mLayout;  // Snackbar 사용하기 위해서는 View가 필요합니다.
+    // (참고로 Toast에서는 Context가 필요했습니다.)
 
     private Handler mHandler;
     Socket socket;
@@ -97,7 +123,74 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // 화면 켜진 상태를 유지합니다.
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
         setContentView(R.layout.activity_main);
+
+        mLayout = findViewById(R.id.layout_main);
+        surfaceView = findViewById(R.id.camera_preview_main);
+
+
+        // 런타임 퍼미션 완료될때 까지 화면에서 보이지 않게 해야합니다.
+        surfaceView.setVisibility(View.GONE);
+
+        Button button = findViewById(R.id.button_main_capture);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                mCameraPreview.takePicture();
+            }
+        });
+
+        if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+
+            int cameraPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+            int writeExternalStoragePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+
+            if ( cameraPermission == PackageManager.PERMISSION_GRANTED
+                    && writeExternalStoragePermission == PackageManager.PERMISSION_GRANTED) {
+                startCamera();
+
+
+            }else {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])
+                        || ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[1])) {
+
+                    Snackbar.make(mLayout, "이 앱을 실행하려면 카메라와 외부 저장소 접근 권한이 필요합니다.",
+                            Snackbar.LENGTH_INDEFINITE).setAction("확인", new View.OnClickListener() {
+
+                        @Override
+                        public void onClick(View view) {
+                            ActivityCompat.requestPermissions( MainActivity.this, REQUIRED_PERMISSIONS,
+                                    PERMISSIONS_REQUEST_CODE);
+                        }
+                    }).show();
+                } else {
+                    // 2. 사용자가 퍼미션 거부를 한 적이 없는 경우에는 퍼미션 요청을 바로 합니다.
+                    // 요청 결과는 onRequestPermissionResult에서 수신됩니다.
+                    ActivityCompat.requestPermissions( this, REQUIRED_PERMISSIONS,
+                            PERMISSIONS_REQUEST_CODE);
+                }
+            }
+        } else {
+
+            final Snackbar snackbar = Snackbar.make(mLayout, "디바이스가 카메라를 지원하지 않습니다.",
+                    Snackbar.LENGTH_INDEFINITE);
+            snackbar.setAction("확인", new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    snackbar.dismiss();
+                }
+            });
+            snackbar.show();
+        }
+
+
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -112,7 +205,6 @@ public class MainActivity extends AppCompatActivity {
         Log.d("test", "db_data : "+db_data);
         Log.d("test", "arrayData : "+arrayData);
 
-
         for(String str: db_data){
             db_data_string += str+" ";
         }
@@ -121,14 +213,11 @@ public class MainActivity extends AppCompatActivity {
         btVoice = findViewById(R.id.voice);
         txVoice = findViewById(R.id.textView);
 
-
         btVoice.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 startVoiceIntent();
             }
-
-
         });
 
         mHandler = new Handler();
@@ -155,8 +244,6 @@ public class MainActivity extends AppCompatActivity {
                     db_data_mood_string += str+" ";
                 }
 
-
-
                 ConnectThread th = new ConnectThread();
                 th.start();
                 //if (et.getText().toString() != null || !et.getText().toString().equals("")){
@@ -168,16 +255,56 @@ public class MainActivity extends AppCompatActivity {
                    // Log.d("temp", "db_data_string is : " + db_data_string);
                     //ConnectThread th = new ConnectThread();
                     //th.start();
-
                 //}
-
             }
         });
-
     }
+
+    void startCamera(){
+        // Create the Preview view and set it as the content of this Activity.
+        mCameraPreview = new CameraPreview(this, this, CAMERA_FACING, surfaceView);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grandResults) {
+        if ( requestCode == PERMISSIONS_REQUEST_CODE && grandResults.length == REQUIRED_PERMISSIONS.length) {
+            boolean check_result = true;
+            for (int result : grandResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    check_result = false;
+                    break;
+                }
+            }
+            if ( check_result ) {
+                startCamera();
+            }
+            else {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])
+                        || ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[1])) {
+                    Snackbar.make(mLayout, "퍼미션이 거부되었습니다. 앱을 다시 실행하여 퍼미션을 허용해주세요. ",
+                            Snackbar.LENGTH_INDEFINITE).setAction("확인", new View.OnClickListener() {
+
+                        @Override
+                        public void onClick(View view) {
+                            finish();
+                        }
+                    }).show();
+                }else {
+                    Snackbar.make(mLayout, "설정(앱 정보)에서 퍼미션을 허용해야 합니다. ",
+                            Snackbar.LENGTH_INDEFINITE).setAction("확인", new View.OnClickListener() {
+
+                        @Override
+                        public void onClick(View view) {
+                            finish();
+                        }
+                    }).show();
+                }
+            }
+        }
+    }
+
+
     public void getFirebaseDatabase(){
-
-
         arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
         //ListView listView = (ListView) findViewById(R.id.db_list_view);
         //listView.setAdapter(arrayAdapter);
@@ -391,10 +518,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     };
-
-
-
-
 
 
 
